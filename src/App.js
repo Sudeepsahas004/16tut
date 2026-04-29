b889c8a6cd3f5cbd367c5535352160da0c848268
Sudeepsahas004 <152478372+Sudeepsahas004@users.noreply.github.com>
2026-04-29 08:51:16 +0530

    Revamp App component with new deployment message
    Updated the App component to display a new heading and message.

--- src/App.js
+++ src/App.js
@@ -3,23 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 My React App Deployed on Azure</h1>
+      <p>This is my first cloud deployment!</p>
     </div>
-  );
 }
 
 export default App;
